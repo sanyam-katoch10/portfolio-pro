09d06add2d7bcba20976239ef7c61e3a2c38e213
Sanyam Katoch <142176459+sanyam-katoch10@users.noreply.github.com>
2026-03-16 12:30:44 +0530

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,20 +9,20 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              RAJESH
+              SANYAM
               <br />
-              <span>CHITYAL</span>
+              <span>KATOCH</span>
             </h1>
           </div>
           <div className="landing-info">
-            <h3>A Full Stack</h3>
+            <h3>A Machine Learning</h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Developer</div>
-              <div className="landing-h2-2">Engineer</div>
+              <div className="landing-h2-1">Engineer</div>
+              <div className="landing-h2-2">Researcher</div>
             </h2>
             <h2>
               <div className="landing-h2-info">Engineer</div>
-              <div className="landing-h2-info-1">Developer</div>
+              <div className="landing-h2-info-1">Researcher</div>
             </h2>
           </div>
         </div>
